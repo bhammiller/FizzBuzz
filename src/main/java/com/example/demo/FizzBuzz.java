4a95cd3f4bf4e01f4648dea96f6020dc485440b2
Brandon Hammiller <bdayton1985@gmail.com>
2018-01-25 15:18:26 -0500

    Finished the assignment plus the bonus parts. Made some minor tweaks

--- src/main/java/com/example/demo/FizzBuzz.java
+++ src/main/java/com/example/demo/FizzBuzz.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class FizzBuzz {
     public String fizzBuzzing(){
-        String output ="";
+        String output;
         Integer counter7 = 0;
         Integer counter17 = 0;
         Integer counter23 = 0;
@@ -11,7 +11,7 @@ public class FizzBuzz {
         System.out.println("Please enter the ending number");
         Integer end_number = keyboard.nextInt();
         output = "The ending number is "+ end_number;
-        System.out.println(output + "\n<br/>" );
+        System.out.println(output);
 
         for (int x=1; x <= end_number; ++x) {
             if(x % 7 == 0 )
@@ -20,15 +20,15 @@ public class FizzBuzz {
                 counter17 += 1;
             if(x % 23 == 0 )
                 counter23 += 1;
-            if ((x % 3 == 0) && (x % 5 == 0)) {
+            if ((x % 3 == 0) && (x % 5 == 0))
                 output += "<br/>" + "FizzBuzz";
-            } else if (x % 3 == 0) {
+             else if (x % 3 == 0)
                 output += "<br/>" + "Fizz";
-            } else if (x % 5 == 0) {
+             else if (x % 5 == 0)
                 output += "<br/>" + "Buzz";
-            } else {
+             else
                 output += "<br/>" + x;
-            }
+
         }
         output += "<br/>" + "The number of muliples of 7 is " + counter7
                 + "<br/>The number of multiples of 17 is " + counter17
