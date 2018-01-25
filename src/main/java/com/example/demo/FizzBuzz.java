d37d059222e27dc5d5e0f296a1393a0b23dd3576
Brandon Hammiller <bdayton1985@gmail.com>
2018-01-25 14:44:40 -0500

    Got it working right.

--- src/main/java/com/example/demo/FizzBuzz.java
+++ src/main/java/com/example/demo/FizzBuzz.java
@@ -3,11 +3,16 @@ package com.example.demo;
 public class FizzBuzz {
     public String fizzBuzzing(){
         String output ="";
-        Integer x = 1;
-        while (x <= 100) {
-           // if ((x % 3 == 0) && )
-            output = output + "<br/>" + x;
-            ++x;
+        for (int x=1; x <= 100; ++x) {
+            if ((x % 3 == 0) && (x % 5 == 0)) {
+                output += "<br/>" + "FizzBuzz";
+            } else if (x % 3 == 0) {
+                output += "<br/>" + "Fizz";
+            } else if (x % 5 == 0) {
+                output += "<br/>" + "Buzz";
+            } else {
+                output += "<br/>" + x;
+            }
         }
         return output;
 
