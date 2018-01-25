3dc6c7c871b90db29cdca3f80adba11637fa345e
Brandon Hammiller <bdayton1985@gmail.com>
2018-01-25 14:28:29 -0500

    got the do while loop working

--- src/main/java/com/example/demo/FizzBuzz.java
+++ src/main/java/com/example/demo/FizzBuzz.java
@@ -2,11 +2,14 @@ package com.example.demo;
 
 public class FizzBuzz {
     public String fizzBuzzing(){
+        String output ="";
         Integer x = 1;
         while (x <= 100) {
-            return "x";
+           // if ((x % 3 == 0) && )
+            output = output + "<br/>" + x;
             ++x;
         }
-        }
+        return output;
+
     }
 }
